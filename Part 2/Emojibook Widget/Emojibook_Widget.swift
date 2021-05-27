68e7aa42f05cd60e3a111928f6a4c1b122ad5a49
Huseyingulecen <gulecenh45@gmail.com>
2021-05-27 23:42:47 +0200

    fix error Type 'Provider'does not conform to protocol 'TimeLineProvider'and Generic parameter 'Content' could not be inferred

--- Part 2/Emojibook Widget/Emojibook_Widget.swift
+++ Part 2/Emojibook Widget/Emojibook_Widget.swift
@@ -9,27 +9,38 @@ import WidgetKit
 import SwiftUI
 
 struct Provider: TimelineProvider {
-  public typealias Entry = SimpleEntry
-
-  public func snapshot(with context: Context, completion: @escaping (SimpleEntry) -> ()) {
-    let entry = SimpleEntry(date: Date(), emojiDetails: EmojiProvider.random())
-    completion(entry)
-  }
-
-  public func timeline(with context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
-    var entries: [SimpleEntry] = []
-
-    // Generate a timeline consisting of five entries an hour apart, starting from the current date.
-    let currentDate = Date()
-    for hourOffset in 0 ..< 5 {
-      let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
-      let entry = SimpleEntry(date: entryDate, emojiDetails: EmojiProvider.random())
-      entries.append(entry)
+    
+    
+    public typealias Entry = SimpleEntry
+    
+    
+    func placeholder(in context: Context) -> SimpleEntry {
+        return SimpleEntry (date: Date(), emojiDetails: EmojiDetails(emoji: " ", name: " " , description: " "))
     }
+    
+    func getSnapshot(in context: Context, completion: @escaping (SimpleEntry) -> Void) {
+        let entry = SimpleEntry(date: Date(), emojiDetails: EmojiProvider.random())
+        completion(entry)
+    }
+    
+    func getTimeline(in context: Context, completion: @escaping (Timeline<SimpleEntry>) -> Void) {
+        var entries: [SimpleEntry] = []
 
-    let timeline = Timeline(entries: entries, policy: .atEnd)
-    completion(timeline)
-  }
+        // Generate a timeline consisting of five entries an hour apart, starting from the current date.
+        let currentDate = Date()
+        for hourOffset in 0 ..< 5 {
+          let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
+          let entry = SimpleEntry(date: entryDate, emojiDetails: EmojiProvider.random())
+          entries.append(entry)
+        }
+
+        let timeline = Timeline(entries: entries, policy: .atEnd)
+        completion(timeline)
+    }
+    
+  
+
+  
 }
 
 struct SimpleEntry: TimelineEntry {
@@ -55,18 +66,18 @@ struct Emojibook_WidgetEntryView: View {
 struct Emojibook_Widget: Widget {
   private let kind: String = "Emojibook_Widget"
 
-  public var body: some WidgetConfiguration {
-    StaticConfiguration(
-      kind: kind,
-      provider: Provider(),
-      placeholder: PlaceholderView()
-    ) { entry in
-      Emojibook_WidgetEntryView(entry: entry)
+    public var body: some WidgetConfiguration {
+      StaticConfiguration(
+        kind: kind,
+        provider: Provider()
+        
+      ) { entry in
+        Emojibook_WidgetEntryView(entry: entry)
+      }
+      .configurationDisplayName("Random Emoji")
+      .description("Display a widget with an emoji that is updated randomly.")
+      .supportedFamilies([.systemSmall,.systemMedium,.systemLarge])
     }
-    .configurationDisplayName("Random Emoji")
-    .description("Display a widget with an emoji that is updated randomly.")
-    .supportedFamilies([.systemSmall, .systemMedium, .systemLarge])
-  }
 }
 
 struct Emojibook_Widget_Previews: PreviewProvider {
